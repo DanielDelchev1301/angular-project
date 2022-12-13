fdb8f2ef2512470cf83314c3f5bb9a6d64ab77c6
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-12-13 10:33:25 +0200

    connect mongo

---
+++ configuration/environment.js
@@ -0,0 +1,2 @@
+exports.CONNECTION_STRING = 'mongodb://localhost:27017/parfumeGallery';
+exports.PORT = 5000;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const routes = require('./routes');
+const { initializeDataBase } = require('./configuration/dataBase');
+const { PORT } = require('./configuration/environment');
 
 const app = express();
 
@@ -9,3 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(routes);
+
+initializeDataBase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+    })
+    .catch(err => {
+        console.log(`Something went wrong: ${err}`);
+    })
